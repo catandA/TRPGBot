f3fb3f51cdd42f804edb51e2362d2fac17450e7a
catandA <75292299+catandA@users.noreply.github.com>
2024-03-18 22:56:47 +0800

    初始化代码

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.2.3"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "me.catand"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    maven("https://maven.aliyun.com/repository/public")
+    maven("https://maven.aliyun.com/repository/spring/")
+    maven("https://jitpack.io")
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // Lombok
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    // Shiro
+    implementation("com.mikuac:shiro:2.1.6")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/java/me/catand/trpgbot/TrpgBotApplication.java
@@ -0,0 +1,13 @@
+package me.catand.trpgbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrpgBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrpgBotApplication.class, args);
+	}
+
+}
